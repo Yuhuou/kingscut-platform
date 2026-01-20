68d69f7505f77b1e5089e3511637a9569c61316f
Yuhuou <joaojosafa11@gmail.com>
2026-01-20 20:39:31 -0300

    Initial commit backend setup

---
+++ src/test/java/com/rls/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rls.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
